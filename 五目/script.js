90087f2912e226c26b2bbbeb12973a6781582b13
wanghuachen <150970520@qq.com>
2021-07-06 14:10:35 +0900

    Default Changelist

---
+++ 五目/script.js
@@ -0,0 +1,30 @@
+const ROWS=15;
+const COLUMNS=15;
+
+const table = document.getElementById("field-table");
+for (let y = 0; y < ROWS; y++) {
+    const row = document.createElement("tr");
+    for (let x = 0; x < COLUMNS; x++) {
+        const box = document.createElement("td");
+        const chips = document.createElement("div");
+        const lastRow = document.createElement("div");
+        const lastColumn = document.createElement("div");
+        const lastRowColumn = document.createElement("div");
+        if (x === 14){
+            lastRow.className="chip last-in-row";
+            box.appendChild(lastRow);
+        }
+        if (y === 14){
+            lastColumn.className="chip last-in-column";
+            box.appendChild(lastColumn);
+        }
+        if (x === 14&&y === 14){
+            lastRowColumn.className="chip last-in-column last-in-row";
+            box.appendChild(lastRowColumn);
+        }
+        chips.className = "chip";
+        box.appendChild(chips);
+        row.appendChild(box);
+    }
+    table.appendChild(row);
+}
